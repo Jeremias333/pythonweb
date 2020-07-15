dc76cc9d7695dc504235329ffb3d7f9112020e4f
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-07-14 22:03:25 -0300

    Adicionado novas rotas para acesso.

--- initial/blog/funcionarios/urls.py
+++ initial/blog/funcionarios/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.home),
-	url(r'^lista/', views.lista)
+	url(r'^lista/', views.lista),
+	url(r'^adicionar/', views.adicionar),
+	url(r'^enviar/', views.send_funcionario)
 ]
