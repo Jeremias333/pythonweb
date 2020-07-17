097f83086d3f92d443d30aa9de638959d9d73a13
Jeremias O. Silva <jeremiasprogrammer@gmail.com>
2020-07-16 22:10:32 -0300

    Adicionado mais rotas

--- initial/blog/funcionarios/urls.py
+++ initial/blog/funcionarios/urls.py
@@ -5,5 +5,10 @@ urlpatterns = [
 	url(r'^$', views.home),
 	url(r'^lista/', views.lista),
 	url(r'^adicionar/', views.adicionar),
-	url(r'^enviar/', views.send_funcionario)
+	url(r'^enviar/', views.send_funcionario),
+	url(r'^deletar/', views.deletar),
+	url(r'^delete/', views.delete),
+	url(r'^editar/', views.editar),
+	url(r'^edite/', views.edite),
+	url(r'^editando/', views.edite_funcionario)
 ]
